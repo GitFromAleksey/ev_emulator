fa58379db5ddeffae91ef4b6dbdf40444a48d272
Aleksey <a_h.79@mail.ru>
2022-11-10 15:12:51 +0700

    - контроллер сконфигурирован как HID устройство

---
+++ MDK-ARM/RTE/_ev_emulator/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ev_emulator' 
+ * Target:  'ev_emulator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
